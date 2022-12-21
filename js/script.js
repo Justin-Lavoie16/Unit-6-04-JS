e00cf8fc5666f172fd19a4cdaf2e3070fb413755
Justin-Lavoie16 <Justin-Lavoie16@users.noreply.github.com>
2022-12-21 15:20:26 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -7,20 +7,19 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/Unit-6-04-JS/sw.js", {
     scope: "/Unit-6-04-JS/",
-  });
+  })
 }
 
-"use strict"
+;("use strict")
 
 window.onload = function () {
+  const params = new URLSearchParams(document.location.search)
+  const radius = params.get("r")
 
-  const params = new URLSearchParams(document.location.search);
-  const radius = params.get("r");
-  
-  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
-  const dimensions = "<ul>\n<li>Radius = " + radius + "</li>\n</ul>";
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+  const dimensions = "<ul>\n<li>Radius = " + radius + "</li>\n</ul>"
   // output
-  document.getElementById("cube").innerHTML = dimensions;
+  document.getElementById("cube").innerHTML = dimensions
   document.getElementById("answer").innerHTML =
-    "The volume is: " + volume.toFixed(2) + " cm³";
-};
+    "The volume is: " + volume.toFixed(2) + " cm³"
+}
